efde8f677f074f5a79961030368baaadcc9464ea
aseldom <a.seldom@gmail.com>
2023-08-14 22:28:28 +0600

    2. Принципы Kiss, Dry и Yagni [#238813]

---
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -0,0 +1,25 @@
+package ru.job4j.kiss;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MaxMin {
+    public <T> T max(List<T> value, Comparator<T> comparator) {
+        return minMax(value, comparator, 1);
+    }
+
+    public <T> T min(List<T> value, Comparator<T> comparator) {
+        return minMax(value, comparator, -1);
+    }
+
+    private  <T> T minMax(List<T> value, Comparator<T> comparator, int minMax) {
+        if (value.isEmpty()) {
+            return null;
+        }
+        T res = value.get(0);
+        for (T v : value) {
+            res = comparator.compare(res, v) * minMax > 0 ? res : v;
+        }
+        return res;
+    }
+}
---
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.kiss;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MaxMinTest {
+
+    MaxMin maxMin = new MaxMin();
+    List<String> list = new ArrayList<>();
+
+    @Test
+    void checkMaxLengthStringThen123() {
+        Comparator<String> comparator = Comparator.comparingInt(String::length);
+        list.add("1");
+        list.add("12");
+        list.add("123");
+        String expect = "123";
+        String res = maxMin.max(list, comparator);
+        assertThat(res).isEqualTo(expect);
+    }
+
+    @Test
+    void checkMaxOneValueThen12() {
+        Comparator<String> comparator = Comparator.comparingInt(String::length);
+        list.add("12");
+        String expect = "12";
+        String res = maxMin.max(list, comparator);
+        assertThat(res).isEqualTo(expect);
+    }
+
+    @Test
+    void checkMaxNoValueThenNull() {
+        Comparator<String> comparator = Comparator.comparingInt(String::length);
+        String expect = null;
+        String res = maxMin.max(list, comparator);
+        assertThat(res).isEqualTo(expect);
+    }
+
+    @Test
+    void checkMinLengthStringThen1() {
+        Comparator<String> comparator = Comparator.comparingInt(String::length);
+        list.add("1");
+        list.add("12");
+        list.add("123");
+        String expect = "1";
+        String res = maxMin.min(list, comparator);
+        assertThat(res).isEqualTo(expect);
+    }
+}
