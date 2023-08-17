8f08184e5ecb7851c134fce86a9389ce1ce7dc11
aseldom <a.seldom@gmail.com>
2023-08-17 13:11:52 +0600

    ----

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -2,20 +2,21 @@ package ru.job4j.kiss;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiPredicate;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        return minMax(value, comparator, 1);
+        return minMax(value, (v1, v2) -> comparator.compare(v1, v2) > 0);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return minMax(value, comparator, -1);
+        return minMax(value, (v1, v2) -> comparator.compare(v1, v2) < 0);
     }
 
-    private  <T> T minMax(List<T> value, Comparator<T> comparator, int minMax) {
+    private  <T> T minMax(List<T> value, BiPredicate<T, T> biPredicate) {
         T res = value.isEmpty() ? null : value.get(0);
         for (T v : value) {
-            res = comparator.compare(res, v) * minMax > 0 ? res : v;
+            res = biPredicate.test(res, v) ? res : v;
         }
         return res;
     }
--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 class MaxMinTest {
 
     MaxMin maxMin = new MaxMin();
